cdf93af24c15b6691526202bce3e3ea0cb37c655
karpa08123 <jmmontesdoreyes@gmail.com>
2024-12-02 05:19:45 -0600

    FUNCIONA TOOD. SOLO QUEDA GENERAR EL XML

--- run.py
+++ run.py
@@ -8,13 +8,12 @@ Todo:
 from flask import Flask, render_template
 from flask import request
 from bd import getDatabase
-from flask import make_response
 from flask import session
 from flask import url_for
 from flask import redirect
+from bson.objectid import ObjectId
 import crud
 import registerForm
-import json
 
 app = Flask(__name__)
 app.secret_key = 'papasconchile'
@@ -34,12 +33,17 @@ def article(idOnWeb):
 
     sellingForm = registerForm.Selling(request.form)
     if request.method == 'POST' and sellingForm.validate() and 'username' in session:
-         session.get('carrito', [])
-         carrito = []
-         artiqlo={'_id':sellingForm.articleId.data, 'quantity':sellingForm.quantity.data}
-         carrito.append(artiqlo)
-         session['carrito'] = carrito
-         print(session['carrito'])
+        if 'carrito' not in session:
+            session.get('carrito', [])
+            print('Se genero un carrito')
+            carrito = []
+        elif 'carrito' in session:
+            carrito = session.get('carrito')
+            print('se extrajo un carrito')
+        artiqlo={'_id':sellingForm.articleId.data, 'quantity':sellingForm.quantity.data}
+        carrito.append(artiqlo)
+        session['carrito'] = carrito
+        print(session['carrito'])
     return render_template("article.html", articulo=arti, form=sellingForm)
 
 @app.route("/register/", methods=['GET','POST'])
@@ -67,6 +71,7 @@ def login():
         validateUser = crud.clearFormat(validateUser)
         if(validateUser):
             if(checkUser['password']==validateUser['password']):
+                session['id']=str(validateUser['_id'])
                 session['username']=checkUser['username']
                 print("Login hecho exitosamente")
      return render_template("login.html", form=formulario)
@@ -99,7 +104,100 @@ def user():
 
 @app.route("/cart/")
 def cart():
-     return render_template("cart.html")
+    query =[]
+    carro = session.get('carrito', [])
+    for item in carro:
+         flag1 = crud.readDocById(db,'producto',item['_id'])
+         flag1 = crud.clearFormat(flag1)
+         query.append(flag1)
+    largo = len(query)
+
+    sumatoria=0
+    for i in range(0,largo,1):
+        sumatoria = sumatoria + ((float(query[i]['precio'])-(float(query[i]['descuento'])*float(query[i]['precio'])))*float(carro[i]['quantity']))
+    iva = sumatoria + (sumatoria*0.16)
+    return render_template("cart.html", bigData=query, littleData=carro, largo=largo, sumatoria=sumatoria, IVA=iva)
+
+@app.route("/checkout/", methods=['GET','POST'])
+def checkout():
+    from datetime import datetime
+    subtotal = request.form.get('total')
+    total = request.form.get('iva')
+
+    productos = []
+    i = 0
+    while True:
+        product_id = request.form.get(f"products[{i}][id]")
+        quantity = request.form.get(f"products[{i}][quantity]")
+        price = request.form.get(f"products[{i}][price]")
+        discount = request.form.get(f"products[{i}][discount]")
+
+        if not product_id:
+            break
+        productos.append({
+            "idArticulo": product_id,
+            "cantidad": int(quantity),
+            "precio": float(price),
+            "descuento": float(discount)
+        })
+        i += 1
+
+    userdata = crud.readDocById(db,'usuarios',session['id'])
+    userdata = crud.clearFormat(userdata)
+    vendedorData = crud.readDocById(db,'usuarios','674d806dbb06e34cda70056c')
+    vendedorData = crud.clearFormat(vendedorData)
+
+    venta = {
+        'idUsuario':str(userdata['_id']),
+        'domicilio':str(userdata['direccion']),
+        'articulos':productos,
+        'subtotal':subtotal,
+        'metodoPago':'efectivo',
+        'totalIva':total
+    }
+
+    crud.createOneDoc(db,'ventas',venta)
+    print("venta generada")
+
+    ventaData = crud.readDoc(db,'ventas',venta)
+    ventaData = crud.clearFormat(ventaData)
+
+    xmlGenerales={
+        'folio':str(ObjectId()),
+        'version':4,
+        'noDeVenta':ventaData['_id'],
+        'fechaEmision':str(datetime.now()),
+        'lugarExpedicion':vendedorData['direccion'],
+    }
+    xmlVenta={
+        'articulos':productos,
+        'subTotal':subtotal,
+        'totalIva':total,
+        'metodoPago':'efectivo'
+    }
+    xmlVendedor={
+        'rfc':vendedorData['rfc'],
+        'nombre':vendedorData['nombre'],
+    }
+
+    compradorNombre = userdata['nombre'] + " "+ userdata['apellido']
+    xmlComprador={
+        'rfc':userdata['rfc'],
+        'nombre':compradorNombre,
+        'domicilio':userdata['direccion']
+    }
+
+    xml={
+        'generales':xmlGenerales,
+        'venta':xmlVenta,
+        'vendedor':xmlVendedor,
+        'comprador':xmlComprador         
+    }
+
+    crud.createOneDoc(db,'xml',xml)
+    print('xml generado!!!!!!!')
+
+    return render_template("checkout.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
--- tests.py
+++ tests.py
@@ -9,7 +9,12 @@ idOnWeb = "6738b34e3ee590aec042f807"
 testFlag = crud.readDoc(db,collection,{})
 
 
-for i in range(0,len(testFlag),1):
-    if (i%4)==0:
-        print('')
-    print(testFlag[i]['_id'])
+from datetime import datetime
+
+#Día actual
+
+#Fecha actual
+now = datetime.now()
+
+
+print(now)
